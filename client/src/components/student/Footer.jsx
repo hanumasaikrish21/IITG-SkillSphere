74f1b668a08653cf758f797d279fe20902a07f28
hanumasaikrish21 <hanumasaikrishna21@gmail.com>
2025-06-07 13:59:31 +0530

    Day-1

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className='bg-white md:px-36 text-left w-full mt-10'>
+      <div className='flex flex-col md:flex-row items-center px-8 md:px-0 justify-center gap-10 md:gap-32  py-10 border-b border-gray-900/30'>
+         <div className='flex flex-col md:items-start items-center w-full'>
+          <img src={assets.logo_dark} alt="logo" />
+          <p className='mt-6 text-center md:text-left text-sm text-black'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
+         </div>
+         <div className='flex flex-col md:items-start items-center w-full'>
+           <h2 className='font-semibold mb-5'>Company</h2>
+           <ul className='flex md:flex-col w-full justify-between text-sm md:space-y-2'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Privacy policy</a></li>
+            
+           </ul>
+         </div>
+         <div className='hidden md:flex flex-col items-start w-full'>
+          <h2 className='font-semibold mb-5'>Subscribe to our newsletter
+          </h2>
+          <p className='text-sm '>The latest news, articles, and resources, sent to your inbox weekly.</p>
+          <div className='flex items-center gap-2 pt-4'>
+            <input type="email" placeholder='enter your email' className='border border-gray-500 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm'/>
+            <button className='bg-limeAccent w-24 h-9 rounded-lg'>Subscribe</button>
+          
+          </div>
+         </div>
+      </div> 
+      <p className='py-4 text-center text-xs md:text-sm '>Copyright 2024 © IITG-SkillSphere. All Right Reserved.</p>
+    </footer>
   )
 }
 
--- client/src/components/student/TestimonialsSection.jsx
+++ client/src/components/student/TestimonialsSection.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
+import { assets, dummyTestimonial } from '../../assets/assets'
 
 const TestimonialsSection = () => {
   return (
-    <div>
-      <h1>TestimonialsSection</h1>
+    <div className='pb-14 px-8 md:px-0'>
+      <h2 className='text-white text-3xl font-medium'>Testimonials</h2>
+      <p className='text-gray-400 md:text-base mt-3'>
+        Hear from our learners as they share their journeys of transformation, success, and how our <br />
+        platform has made a difference in their lives.
+      </p>
+
+      <div className='grid grid-cols-auto gap-8 mt-14'>
+        {dummyTestimonial.map((testimonial, index) => (
+          <div
+            key={index}
+            className='text-s text-left border border-black  pb-6  shadow-black/5 overflow-hidden'
+          >
+            <div className='flex items-center gap-4 px-5 py-4 bg-black'>
+              <img className='h-12 w-12 rounded-full' src={testimonial.image} alt={testimonial.name} />
+              <div>
+                <h1 className='text-lg font-medium  text-white'>{testimonial.name}</h1>
+                <p className='text-gray-400'>{testimonial.role}</p>
+              </div>
+            </div>
+
+            <div className='p-5 pb-7'>
+              <div className='flex gap-0.5'>
+                {[...Array(5)].map((_, i) => (
+                  <img
+                    className='h-5'
+                    key={i}
+                    src={i < Math.floor(testimonial.rating) ? assets.star : assets.star_blank}
+                    alt="star"
+                  />
+                ))}
+              </div>
+              <p className='text-gray-400 mt-5'>{testimonial.feedback}</p>
+            </div>
+            <a href="#" className='text-white hover:text-limeAccent  px-5'>Read more...</a>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default TestimonialsSection
-
-        
