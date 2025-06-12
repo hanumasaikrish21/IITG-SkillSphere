e6b5d000a10e309e8cff482f735f21578338c106
hanumasaikrish21 <hanumasaikrishna21@gmail.com>
2025-06-12 13:22:07 +0530

    Day-06:Front-end:Completed

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyStudentEnrolled } from '../../assets/assets'
+import Loading from '../../components/student/Loading'
 
 const StudentsEnrolled = () => {
-  return (
-    <div>
-      <h1 className='text-white'>StudentsEnrolled</h1>
+  const [enroledStudents,setEnroledStudents]=useState(null)
+  const fetchenroledStdents=async()=>{
+    setEnroledStudents(dummyStudentEnrolled)
+  }
+  useEffect(()=>{
+    fetchenroledStdents()
+  },[])
+  return enroledStudents? (
+    <div className='min-h-screen flex flex-col  items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0 '>
+     <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md border shadow-limeGlow border-limeAccent'>
+      <table className='table-fixed md:table-auto w-full overflow-hidden pb-4 '>
+        <thead className='text-white border-b text-sm text-left border-limeAccent'>
+          <tr>
+            <th className='px-4 py-3 font-semibold'>#</th>
+            <th className='px-4 py-3 font-semibold'>Student Name</th>
+            <th className='px-4 py-3 font-semibold'>Course Title</th>
+            <th className='px-4 py-3 font-semibold hidden sm:table-cell'>Date</th>
+          </tr>
+
+        </thead>
+        <tbody className='text-white'>
+          {enroledStudents.map((item,index)=>(
+            <tr key={index} className='border-b border-limeAccent'>
+            <td className='px-4 py-3 text-center hidden sm:table-cell'>{index+1}</td>
+            <td className='md:px-4 py-3 px-2 flex items-center space-x-3' >
+              <img src={item.student.imageUrl} alt="" className='w-9 h-9 rounded-full'/>
+              <span className='truncate'>{item.student.name}</span>
+            </td>
+            <td className='px-4 py-3 truncate'>{item.courseTitle}</td>
+            <td className='px-4 py-3 hidden sm:table-cell'>{ new Date(item.purchaseDate).toLocaleDateString()}</td>
+          </tr>
+
+          ))}
+          
+        </tbody>
+      </table>
+     </div>
     </div>
-  )
+  ):<Loading/>
 }
 
 export default StudentsEnrolled
