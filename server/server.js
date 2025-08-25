d24e26706eb6b24a45ed85653a7d94f3f972dd27
Hanuma Sai Krishna <hanumasaikrishna21@gmail.com>
2025-08-25 23:43:40 +0530

    Configure CORS with dynamic frontend URL

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ await connectCloudinary();
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
 
 // ====== JSON BODY FOR EVERYTHING ELSE ======
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(clerkMiddleware());
 
